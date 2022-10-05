e3a682c07e7ea7dbcb2d2246ec0c07731815605b
AndrewKnow <92088740+AndrewKnow@users.noreply.github.com>
2022-10-05 16:41:00 +0300

    Добавьте файлы проекта.

---
+++ ДЗ_9_WPF/ImageDownloader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+
+
+
+namespace ДЗ_9_WPF
+{
+    public delegate void ImageStarted();
+    public delegate void ImageCompleted();
+
+    public class ImageDownloader
+    {
+
+        public event ImageStarted Begin;
+        public event ImageCompleted Finish;
+
+        public void Download()
+        {
+
+            if (Begin != null)
+            {
+                Begin();
+            }
+
+            // Откуда будем качать
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            // Как назовем файл на диске
+            string fileName = "bigimage.jpg";
+
+            // Качаем картинку в текущую директорию
+            var myWebClient = new WebClient();
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            myWebClient.DownloadFile(remoteUri, fileName);
+
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+
+            if (Finish != null)
+            {
+                Finish();
+            }
+
+        }
+
+        public async Task DownloadAsync()
+        {
+
+            if (Begin != null)
+            {
+                Begin();
+            }
+
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+            string fileName = "bigimage.jpg";
+            var myWebClient = new WebClient();
+
+            Console.WriteLine("Качаю асинхронно \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            await myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+
+            Console.WriteLine("Успешно скачал асинхронно\"{0}\" из \"{1}\"", fileName, remoteUri);
+
+
+            if (Finish != null)
+            {
+                Finish();
+            }
+
+        }
+
+
+    }
+}
---
+++ ДЗ_9_WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ДЗ_9_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public Task result { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+
+            ImageDownloader imageDownloader = new ImageDownloader();
+
+            imageDownloader.Begin += ImageDownloader_Begin;
+            imageDownloader.Finish += ImageDownloader_Finish;
+
+            // imageDownloader.Download();
+            result = imageDownloader.DownloadAsync();
+
+
+        }
+
+        private void Status_Click(object sender, RoutedEventArgs e)
+        {
+            if (result.IsCompleted == true)
+            {
+                StatusTXT_Copy.Text = result.IsCompleted.ToString();
+            }
+            else
+            {
+                StatusTXT_Copy.Text = result.IsCompleted.ToString();
+            }
+        }
+
+        private void ImageDownloader_Begin()
+        {
+
+            StatusTXT.Text = "Скачивание файла началось";
+
+
+        }
+
+        private void ImageDownloader_Finish()
+        {
+
+            StatusTXT.Text = "Скачивание файла закончилось";
+
+        }
+    }
+}
